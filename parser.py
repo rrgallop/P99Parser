2840bde2c2017b8e445522e7c6dacd6540c445f5
rrgallop <ryan.gallop@maine.edu>
2019-10-20 12:09:27 -0400

    saving what works, bout to go crazy

--- parser.py
+++ parser.py
@@ -1,23 +1,69 @@
 import os
 import sys
+import datetime
 import json
 from glob import glob
 
-from PyQt5.QeCore import QFileSystemWatcher, pyqtSignal
+from PyQt5.QtCore import QFileSystemWatcher, pyqtSignal
 
-eq_directory = r'C:\Users\User\Desktop\school\stuff\EverQuest\Logs'
+data = {}
+config_file = r'config.json'
+
+
+def strip_timestamp(line):
+    """
+    Strings EQ Timestamp from log entry.
+    """
+    return line[line.find("]") + 1:].strip()
+
+class LogReader(QFileSystemWatcher):
+    print("Sup")
+    new_line = pyqtSignal(object)
+    def __init__(self,dir):
+        self.logfiles = glob(os.path.join(dir, 'eqlog*.txt'))
+        self.filesys_watcher = QFileSystemWatcher(self.logfiles)
+        self.filesys_watcher.fileChanged.connect(self.file_changed)
+
+        self.stats = {
+            'log_file': '',
+            'last_read': 0
+        }
+
+    def file_changed(self, changed_file):
+        with open(changed_file) as log:
+
+            if changed_file != self.stats['log_file']:
+                self.stats['log_file'] = changed_file
+                log.seek(0, os.SEEK_END)
+                self.stats['last_read'] = log.tell()
+            try:
+                log.seek(self.stats['last_read'], os.SEEK_SET)
+                lines = log.readlines()
+                self.stats['last_read'] = log.tell()
+                for line in lines:
+                    self.new_line.emit((
+                        datetime.datetime.now(),
+                        strip_timestamp(line)
+
+                    ))
+            except Exception:
+                log.seek(0, os.SEEK_END)
+                self.stats['last_read'] = log.tell()
+
 
 class EQParser:
 
-    def __init__(self,*args):
-        super().__init__(*args)
-        self.log_reader = LogReader(eq_directory)
+    def __init__(self):
+        self.load_config(config_file)
+        self.log_reader = LogReader(data['eq_log_dir'])
+        self.spellbook = create_spell_book()
 
-class LogReader:
-    def __init__(self,dir):
-        super().__init__()
-        self.logfiles = glob(os.path.join(dir, 'eqlog*.txt'))
-        self.filesys_watcher = QFileSystemWatcher(self.logfiles)
+    def load_config(self,filename):
+        global data
+        file = filename
+        with open(file,'r+') as f:
+                data = json.loads(f.read())
+
 
 class Spell:
     def __init__(self, **kwargs):
@@ -37,7 +83,6 @@ class Spell:
         self.type = 0
         self.spell_icon = 0
         self.__dict__.update(kwargs)
-
 
 
 def create_spell_book():
@@ -70,4 +115,6 @@ def create_spell_book():
         return spellbook
 
 
-create_spell_book()
+bruh = EQParser()
+print(bruh.log_reader.logfiles)
+print(bruh.spellbook)
